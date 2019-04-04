60a4d4770eabf522341869501919cdc1c08d78fd
Jeff Hobson <jeffrey296@gmail.com>
2019-04-04 15:52:58 -0400

    Include registration close dates in useful places
    Also make home page update based on whether registration is open.

--- src/app/pre-event-home-page/pre-event-home-page.component.ts
+++ src/app/pre-event-home-page/pre-event-home-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 
+import { SpeedrunEvent, SpeedrunEventService } from '../speedrun-event.service'
+
 @Component({
 	selector: 'app-pre-event-home-page',
 	templateUrl: './pre-event-home-page.component.html',
@@ -8,10 +10,15 @@ import { Component, OnInit } from '@angular/core'
 export class PreEventHomePageComponent implements OnInit {
 	// TODO: Don't hardcode this
 	event: string = 'RPG Limit Break 2019'
+	speedrunEvent: SpeedrunEvent
 
-	constructor() { }
+	constructor(private speedrunEventService: SpeedrunEventService) { }
 
 	ngOnInit() {
+		this.speedrunEventService.getCurrentSpeedrunEvent()
+			.subscribe((srEvent: SpeedrunEvent) => {
+				this.speedrunEvent = srEvent
+			})
 	}
 
 }
