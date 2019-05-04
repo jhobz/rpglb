fa73257723144b34609ba9c74afba117395fa911
Jeff Hobson <jeffrey296@gmail.com>
2019-05-04 16:34:00 -0600

    Update rules page for 2019
    Also make donation banner apply to all pages globally
    
    Closes #49

--- src/app/donate-header/donate-header.component.ts
+++ src/app/donate-header/donate-header.component.ts
@@ -1,16 +1,38 @@
+import { HttpErrorResponse } from '@angular/common/http'
 import { Component, Input, OnInit } from '@angular/core'
+
+import { DonationService } from '../donation.service'
+import { DonationData, SpeedrunEvent } from '../speedrun-event'
 
 @Component({
 	selector: 'app-donate-header',
 	templateUrl: './donate-header.component.html',
-	styleUrls: ['./donate-header.component.scss']
+	styleUrls: ['./donate-header.component.scss'],
+	providers: [DonationService]
 })
 export class DonateHeaderComponent implements OnInit {
-	@Input() event: any
+	event: SpeedrunEvent = new SpeedrunEvent()
 
-	constructor() { }
+	constructor(private donationService: DonationService) {
+		this.event.name = 'RPG Limit Break 2019'
+		this.event.shortName = 'rpglb2018'
+		this.event.cause = 'NAMI: National Alliance on Mental Illness'
+		this.event.causeLink = 'https://www.nami.org'
+		this.event.trackerId = 7
+		this.event.donations = { total: 50000, goal: 100000 }
+	}
 
 	ngOnInit() {
+		this.donationService.getTotalForEvent(this.event)
+			.subscribe(
+				(total: string) => this.event.donations.total = Number(total),
+				(error: HttpErrorResponse) => console.error('Error getting donation total', error)
+			)
+		this.donationService.getGoalForEvent(this.event)
+			.subscribe(
+				(goal: string) => this.event.donations.goal = Number(goal),
+				(error: HttpErrorResponse) => console.error('Error getting donation goal', error)
+			)
 	}
 
 }
