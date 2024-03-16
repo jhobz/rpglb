9def13f697e4be29ab0017b29ffe1ac28a104999
Jeff Hobson <jeffrey296@gmail.com>
2024-03-15 22:00:24 -0400

    Update volunteer page for 2024

--- src/app/volunteer-page/volunteer-page.component.ts
+++ src/app/volunteer-page/volunteer-page.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit } from "@angular/core"
+
+import { SpeedrunEvent, SpeedrunEventService } from "../speedrun-event.service"
 
 @Component({
-  selector: 'app-volunteer-page',
-  templateUrl: './volunteer-page.component.html',
-  styleUrls: ['./volunteer-page.component.scss']
+	selector: "app-volunteer-page",
+	templateUrl: "./volunteer-page.component.html",
+	styleUrls: ["./volunteer-page.component.scss"],
 })
 export class VolunteerPageComponent implements OnInit {
+	speedrunEvent: SpeedrunEvent
+  closeDate: Date
+  scheduleDate: Date
 
-  constructor() { }
+	constructor(private speedrunEventService: SpeedrunEventService) { }
 
-  ngOnInit() {
+	ngOnInit() {
+		this.speedrunEventService.getCurrentSpeedrunEvent()
+			.subscribe((srEvent: SpeedrunEvent) => {
+				this.speedrunEvent = srEvent
+        this.closeDate = new Date(srEvent.dates.volunteers.applicationsClose)
+        this.scheduleDate = new Date(srEvent.dates.volunteers.scheduleRelease)
+			})
   }
-
 }
