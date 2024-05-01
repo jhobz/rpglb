6e468fa683454ac4f614fdd5343ea2668ab66e63
Jeff Hobson <jeffrey296@gmail.com>
2024-04-30 22:34:30 -0400

    Add store link and remove irrelevant warnings

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,41 +1,44 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit } from "@angular/core";
 
-import { AuthenticationService } from '../authentication.service'
-import { SpeedrunEvent, SpeedrunEventService } from '../speedrun-event.service'
+import { AuthenticationService } from "../authentication.service";
+import { SpeedrunEvent, SpeedrunEventService } from "../speedrun-event.service";
 
 @Component({
-	selector: 'app-navbar',
-	templateUrl: './navbar.component.html',
-	styleUrls: ['./navbar.component.css']
+	selector: "app-navbar",
+	templateUrl: "./navbar.component.html",
+	styleUrls: ["./navbar.component.css"],
 })
 export class NavbarComponent implements OnInit {
-	// TODO: Don't hardcode these
-	mode: 'pre'|'live'|'post'
-	hasSubmissionRole: boolean
-	speedrunEvent: SpeedrunEvent
+	mode: "pre" | "live" | "post";
+	hasSubmissionRole: boolean;
+	speedrunEvent: SpeedrunEvent;
 
 	constructor(
 		private auth: AuthenticationService,
 		private speedrunEventService: SpeedrunEventService
-	) { }
+	) {}
 
 	ngOnInit() {
-		const user = this.auth.getUserInfo()
-		this.hasSubmissionRole = user && user.roles && (user.roles.includes('submissions') || user.roles.includes('admin'))
+		const user = this.auth.getUserInfo();
+		this.hasSubmissionRole =
+			user &&
+			user.roles &&
+			(user.roles.includes("submissions") ||
+				user.roles.includes("admin"));
 
-		this.speedrunEventService.getCurrentSpeedrunEvent()
+		this.speedrunEventService
+			.getCurrentSpeedrunEvent()
 			.subscribe((srEvent: SpeedrunEvent) => {
-				this.speedrunEvent = srEvent
-				this.mode = this.speedrunEvent.state
-			})
+				this.speedrunEvent = srEvent;
+				this.mode = this.speedrunEvent.state;
+			});
 	}
 
 	logout() {
-		this.auth.logout()
+		this.auth.logout();
 	}
 
 	isLoggedIn(): boolean {
-		return this.auth.isLoggedIn()
+		return this.auth.isLoggedIn();
 	}
-
 }
