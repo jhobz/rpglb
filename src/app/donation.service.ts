84968ed597fc553d2cea232c87d3ef65d97c1978
Jeff Hobson <jeffrey296@gmail.com>
2024-05-18 17:30:43 -0400

    Remove old conflicting SpeedrunEvent and hardcoded values on live page
    FINALLY got rid of that old duplicate SpeedrunEvent type, which was
    causing grief on several pages.

--- src/app/donation.service.ts
+++ src/app/donation.service.ts
@@ -1,31 +1,31 @@
-import { HttpClient } from '@angular/common/http'
-import { Injectable } from '@angular/core'
-import 'rxjs/add/operator/map'
-import { Observable } from 'rxjs/Observable'
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import "rxjs/add/operator/map";
+import { Observable } from "rxjs/Observable";
 
-import { SpeedrunEvent } from './speedrun-event'
-import { TrackerJson } from './tracker-json'
+import { SpeedrunEvent } from "./speedrun-event.service";
+import { TrackerJson } from "./tracker-json";
 
 @Injectable()
 export class DonationService {
 	// TODO: Should be read from an environment variable
-	private trackerUrl: string = 'https://tracker.rpglimitbreak.com/event/'
+	private trackerUrl: string = "https://tracker.rpglimitbreak.com/event/";
 
-	constructor(private http: HttpClient) {
-	}
+	constructor(private http: HttpClient) {}
 
 	getTotalForEvent(event: SpeedrunEvent): Observable<string> {
-		return this.http.get(this.trackerUrl + event.trackerId + '?json')
+		return this.http
+			.get(this.trackerUrl + event.trackerId + "?json")
 			.map((data: TrackerJson) => {
 				return data.agg.amount;
 			});
 	}
 
 	getGoalForEvent(event: SpeedrunEvent): Observable<string> {
-		return this.http.get(this.trackerUrl + event.trackerId + '?json')
+		return this.http
+			.get(this.trackerUrl + event.trackerId + "?json")
 			.map((data: TrackerJson) => {
 				return data.agg.target;
 			});
 	}
-
 }
--- src/app/event-sweepstakes/event-sweepstakes.component.ts
+++ src/app/event-sweepstakes/event-sweepstakes.component.ts
@@ -1,40 +1,12 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-
-import { DonationService } from '../donation.service';
-import { DonationData, SpeedrunEvent } from '../speedrun-event';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-	selector: 'app-event-sweepstakes',
-	templateUrl: './event-sweepstakes.component.html',
-	styleUrls: ['./event-sweepstakes.component.css'],
-	providers: [DonationService]
+	selector: "app-event-sweepstakes",
+	templateUrl: "./event-sweepstakes.component.html",
+	styleUrls: ["./event-sweepstakes.component.css"],
 })
 export class EventSweepstakesComponent implements OnInit {
-	event: SpeedrunEvent = new SpeedrunEvent();
-	isChatOpen: boolean = false;
+	constructor() {}
 
-	constructor(private donationService: DonationService) {
-		// TODO: #150 MAKE THIS ACTUALLY PULL FROM THE SPEEDRUN EVENT WTH
-		// this.event.name = 'RPG Limit Break 2018';
-		// this.event.shortName = 'rpglb2018';
-		// this.event.cause = 'NAMI: National Alliance on Mental Illness';
-		// this.event.causeLink = 'https://www.nami.org';
-		// this.event.trackerId = 6;
-		// this.event.donations = { total: 0, goal: 0 };
-	}
-
-	ngOnInit() {
-		this.donationService.getTotalForEvent(this.event)
-			.subscribe(
-				(total: string) => this.event.donations.total = Number(total),
-				(error: HttpErrorResponse) => console.error('Error getting donation total', error)
-			);
-		this.donationService.getGoalForEvent(this.event)
-			.subscribe(
-				(goal: string) => this.event.donations.goal = Number(goal),
-				(error: HttpErrorResponse) => console.error('Error getting donation goal', error)
-			);
-	}
-
+	ngOnInit() {}
 }
